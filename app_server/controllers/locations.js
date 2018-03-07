999718a7245c601531418b131809c9a1fd85a0e2
Viktorych <ovrudeenko@gmail.com>
2018-03-07 14:29:53 +0300

    2

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,9 @@
+module.exports.homelist= function (req,res) {
+    res.render('locations-list',{title :'ДОМ'})
+};
+module.exports.locationInfo= function (req,res) {
+    res.render('location-info',{title :'О местоположении'})
+};
+module.exports.addReview= function (req,res) {
+    res.render('index',{title :'Добавить местоположение'})
+};
