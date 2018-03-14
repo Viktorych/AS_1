5be6808519641a224bc127c4a16430c5b4e2767b
viktorych <ovrudeenko@gmail.com>
2018-03-14 13:50:23 +0300

    3
    Signed-off-by: viktorych <ovrudeenko@gmail.com>

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,31 @@
 module.exports.homelist= function (req,res) {
-    res.render('locations-list',{title :'ДОМ'})
+    res.render('locations-list',{
+        title :'Система оперативного учета',
+        pageHeader:{
+            title:'Система оперативного учета.',
+            stripline:'Поиск запчастей'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+    }, {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+    }, {
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+    }]
+    })
 };
 module.exports.locationInfo= function (req,res) {
     res.render('location-info',{title :'О местоположении'})
