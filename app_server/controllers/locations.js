101eed0c41fd1d8afc7e1ef99f0c3f46cfa31932
viktorych <ovrudeenko@gmail.com>
2018-03-14 15:10:01 +0300

    3
    Signed-off-by: viktorych <ovrudeenko@gmail.com>

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,35 +1,35 @@
-module.exports.homelist= function (req,res) {
-    res.render('locations-list',{
-        title :'Система оперативного учета',
-        pageHeader:{
-            title:'Система оперативного учета.',
-            stripline:'Поиск запчастей'
+module.exports.homelist = function (req, res) {
+    res.render('locations-list', {
+        title: 'Система оперативного учета',
+        pageHeader: {
+            title: 'Система оперативного учета.',
+            stripline: 'Поиск запчастей'
         },
-        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        sidebar: "Пример интерфейса",
         locations: [{
-        name: 'Starcups',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 3,
-        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        distance: '100m'
-    }, {
-        name: 'Cafe Hero',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 4,
-        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-        distance: '200m'
-    }, {
-        name: 'Burger Queen',
-        address: '125 High Street, Reading, RG6 1PS',
-        rating: 2,
-        facilities: ['Food', 'Premium wifi'],
-        distance: '250m'
-    }]
+            name: 'Объект учета 1',
+            address: 'Склад 1',
+            rating: 5,
+            facilities: ['Электрика', 'Двигатели', ''],
+            distance: '1 шт.'
+        }, {
+            name: 'Объект учета 2',
+            address: 'Склад 2',
+            rating: 5,
+            facilities: ['Гидравлика', 'гидростанция', ''],
+            distance: '1 шт.'
+        }, {
+            name: 'Kабель',
+            address: 'Склад 1',
+            rating: 2,
+            facilities: ['Электрика', 'Кабели', ''],
+            distance: '100 м.'
+        }]
     })
 };
-module.exports.locationInfo= function (req,res) {
-    res.render('location-info',{title :'О местоположении'})
+module.exports.locationInfo = function (req, res) {
+    res.render('location-info', {title: 'О местоположении'})
 };
-module.exports.addReview= function (req,res) {
-    res.render('location-review-form',{title :'Добавить местоположение'})
+module.exports.addReview = function (req, res) {
+    res.render('location-review-form', {title: 'Добавить местоположение'})
 };
